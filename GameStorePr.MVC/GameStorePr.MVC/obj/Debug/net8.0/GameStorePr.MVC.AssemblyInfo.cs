cad7e18f5e45edebd76b707d1ea6bd4165605db2
Ravan <ravanna-ab205@code.edu.az>
2024-12-13 18:10:19 +0400

    task added

--- GameStorePr.MVC/GameStorePr.MVC/obj/Debug/net8.0/GameStorePr.MVC.AssemblyInfo.cs
+++ GameStorePr.MVC/GameStorePr.MVC/obj/Debug/net8.0/GameStorePr.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStorePr.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94e09f2f76d9085c3a74644c602966802e8bf67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62b42a28e48caecb7957756f32819b409f83588")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStorePr.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStorePr.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
